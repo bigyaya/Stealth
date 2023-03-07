711432fd255bc971cc2d6084cd9f5e5f949f6976
nasso <73773951+bigyaya@users.noreply.github.com>
2023-03-07 13:51:42 +0100

    ajout de l'animation du saut et de la course
    le joueur peux se déplacer en courant mais la rotation de la caméra change les animations

--- Assets/Scripts/animationStateControler.cs
+++ Assets/Scripts/animationStateControler.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class animationStateControler : MonoBehaviour
 {
     Animator animator;
+    //ThirdPersonMovementV2 controller;
+
+    //float SpeedX = 0.0f;
+    //float SpeedY = 0.0f;
+    //public float acceleration = 2.0f;
+    //public float deceleration = 2.0f;
+    //public bool isJumping = false;
+
+
+
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,26 +28,43 @@ public class animationStateControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //walk
-        if (Input.GetKey("z"))
-        {
-            animator.SetBool("IsWalking", true);
-        }
-        else
-        {
-            animator.SetBool("IsWalking", false);
 
-        }
 
-        //Jogging
-        if (Input.GetKey("left shift"))
-        {
-            animator.SetBool("IsJogging", true);
-        }
-        else
-        {
-            animator.SetBool("IsJogging", false);
 
-        }
+        ////récupère les inputs
+        //bool forwardPressed = Input.GetKey("z");
+        //bool leftPressed = Input.GetKey("q");
+        //bool rightPressed = Input.GetKey("d");
+        //bool backwardPressed = Input.GetKey("s");
+        //bool runPressed = Input.GetKey("left shift");
+
+        ////si le joueur appuie sur "z", augmente la vélocité sur la direction z.
+        //if (forwardPressed)
+        //{
+        //    SpeedY += Time.deltaTime * acceleration;
+        //    animator.SetFloat("SpeedY", SpeedY);
+
+        //}
+        //if (leftPressed)
+        //{
+        //    SpeedX -= Time.deltaTime * acceleration;
+        //}
+        //if (rightPressed)
+        //{
+        //    SpeedX += Time.deltaTime * acceleration;
+        //}
+        //if (backwardPressed)
+        //{
+        //    SpeedY -= Time.deltaTime * acceleration;
+        //}
+
+        //animator.SetFloat("SpeedY", SpeedY);
+        //animator.SetFloat("SpeedX", SpeedX);
+
     }
+
+    
+    
+        
+    
 }
